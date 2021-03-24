4d99d5cad8d21a93698bfe6ce167c01cda3d810f
yvettehenderson <yvette.henderson@gmail.com>
2021-03-24 12:10:23 -0700

    port update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT||3000;
+const PORT = 3000;
 
 const app = express();
 
@@ -28,6 +28,6 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
